06f25fd413123e4c20ba7bad3e6332093a50771b
prateekpm123 <prateekpm123@gmail.com>
2020-05-29 21:49:07 +0530

    Added Jquery library

--- pages/login/login.php
+++ pages/login/login.php
@@ -1,26 +1,19 @@
-<?php 
-
+<?php
 // THIS ENABLES TYPE DECLARATION IN PHP, WHICH AVAILABLE IN LABGUAGES LIKE DART
 declare( strict_types = 1 );            
-include 'includes/class-autoLoad.inc.php';
-
+include __DIR__.'/../../includes/class-autoLoad.inc.php';
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
+    <a href="../admin_login/admin_login.php" class="btn btn-primary">Admin Login</a>
+    <a href="../user_login/user_login.php" class="btn btn-primary">User Login</a>
 
-    <?php 
-
-       header('/pages/login/login.php');
-
-    ?>
-    
 </body>
 </html>

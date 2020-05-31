e39d7a96a42d466a615addc0b728fdf10e379458
prateekpm123 <prateekpm123@gmail.com>
2020-05-31 16:18:40 +0530

    Added jquery to this page

--- pages/login/login.php
+++ pages/login/login.php
@@ -17,12 +17,13 @@ include __DIR__.'/../../includes/class-autoLoad.inc.php';
     ?>
 </head>
 <body>
-    <a href="/../admin_login/admin_login.php" class="btn btn-primary">Admin Login</a>
-    <a href="/../user_login/user_login.php" class="btn btn-primary">User Login</a>
+    <a href="../admin_login/admin_login.php" class="btn btn-primary">Admin Login</a>
+    <a href="../user_login/user_login.php" class="btn btn-primary">User Login</a>
 
-    <!-- Bootstrap js links from constants folder -->
+    <!-- Bootstrap js and jquery links from constants folder -->
     <?php 
         include_once __DIR__.'/../../includes/constants/bootstrapjs.php';
+        include_once __DIR__.'/../../includes/constants/jqueryLinks.php';
     ?>
 </body>
 </html>

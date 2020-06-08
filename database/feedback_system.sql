571df3a0768a2e9df66cde4005a0aad07fd428c8
poojatripathi00030 <poojatripathi002@gmail.com>
2020-06-08 17:28:27 +0530

    UPDATED DATABASE WITH ACCESS TABLE

--- database/feedback_system.sql
+++ database/feedback_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 31, 2020 at 11:48 AM
+-- Generation Time: Jun 08, 2020 at 01:52 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.1.33
 
@@ -25,19 +25,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Table structure for table `access`
 --
 
-CREATE TABLE `admin` (
+CREATE TABLE `access` (
+  `Access_id` int(16) NOT NULL,
+  `F_id` int(16) NOT NULL,
+  `Admin_id` varchar(64) NOT NULL,
+  `access_giver` varchar(255) NOT NULL,
+  `access_receiver` varchar(255) NOT NULL,
+  `priviliges` varchar(255) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `DELETED` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_credentials`
+--
+
+CREATE TABLE `admin_credentials` (
   `A_id` int(16) NOT NULL,
   `Admin_id` varchar(64) NOT NULL,
   `Admin_name` varchar(255) NOT NULL,
   `Admin_email` varchar(255) NOT NULL,
   `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `password` varchar(255) NOT NULL,
+  `Admin_Password` varchar(255) NOT NULL,
   `re_enter_password` varchar(255) NOT NULL,
   `U_id` int(16) NOT NULL,
+  `DELETED` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_credentials`
+--
+
+INSERT INTO `admin_credentials` (`A_id`, `Admin_id`, `Admin_name`, `Admin_email`, `created_on`, `updated_on`, `Admin_Password`, `re_enter_password`, `U_id`, `DELETED`) VALUES
+(1, 'PRA', 'Prateek', 'prateek.manta@sakec.ac.in', '2020-05-31 11:04:35', '2020-05-31 11:04:35', '1234567', '1234567', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_form_access`
+--
+
+CREATE TABLE `admin_form_access` (
+  `Admin_form_id` int(16) NOT NULL,
+  `Access_id` int(16) NOT NULL,
+  `F_id` int(16) NOT NULL,
+  `Admin_id` varchar(64) NOT NULL,
   `DELETED` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -71,11 +110,27 @@ CREATE TABLE `form` (
   `Form_code` varchar(255) NOT NULL,
   `Form_name` varchar(255) NOT NULL,
   `Form_version` varchar(255) NOT NULL,
+  `Form_Desc` varchar(255) NOT NULL,
   `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `Form_details` int(16) NOT NULL,
   `DELETED` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `form`
+--
+
+INSERT INTO `form` (`F_id`, `Admin_id`, `Form_code`, `Form_name`, `Form_version`, `Form_Desc`, `created_on`, `updated_on`, `Form_details`, `DELETED`) VALUES
+(1, 'PRA', '1', 'TEst form 1', '1', '', '2020-06-07 14:37:55', '2020-06-08 06:16:00', 1, 0),
+(2, 'PRA', '2', 'TEst form 2', '1', '', '2020-06-07 14:39:16', '2020-06-07 14:39:16', 1, 0),
+(3, 'PRA', '3', 'TEst form 2', '2', '', '2020-06-07 14:41:46', '2020-06-07 14:41:46', 1, 0),
+(4, 'PRA', '4', 'TEst form 2', '3', '', '2020-06-07 14:41:46', '2020-06-07 14:41:46', 1, 0),
+(5, 'PRA', '5', 'TEst form 2', '4', '', '2020-06-07 14:41:46', '2020-06-07 14:41:46', 1, 0),
+(6, 'PRA', '6', 'TEst form 3', '1', '', '2020-06-07 14:41:46', '2020-06-07 14:41:46', 1, 0),
+(7, 'PRA', '7', 'TEst form 3', '2', '', '2020-06-07 14:41:46', '2020-06-07 14:41:46', 1, 0),
+(8, 'PRA', '8', 'TEst form 3', '3', '', '2020-06-07 14:41:47', '2020-06-07 14:41:47', 1, 0),
+(9, 'PRA', '9', 'TEst form 4', '1', '', '2020-06-07 14:41:47', '2020-06-07 14:41:47', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -91,6 +146,13 @@ CREATE TABLE `form_details` (
   `Access_level` mediumtext NOT NULL,
   `DELETED` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `form_details`
+--
+
+INSERT INTO `form_details` (`Form_details`, `Session`, `Validity`, `Form_editor`, `Access_level`, `DELETED`) VALUES
+(1, '20', '2020-06-23', 'Prateek', 'Master', 0);
 
 -- --------------------------------------------------------
 
@@ -135,6 +197,13 @@ CREATE TABLE `user` (
   `DELETED` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`U_id`, `User_id`, `User_name`, `User _email`, `Role`, `created_on`, `updated_on`, `Details_id`, `Password_id`, `DELETED`) VALUES
+(1, 'PRA', 'prateek', 'prateek.manta@sakec.ac.in', 'Admin', '2020-05-31 11:03:21', '2020-05-31 11:03:21', 1, 1, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -152,6 +221,14 @@ CREATE TABLE `user_details` (
   `DELETED` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `user_details`
+--
+
+INSERT INTO `user_details` (`Details_id`, `First_name`, `Middle_name`, `Last_name`, `Phone`, `created_on`, `updated_on`, `DELETED`) VALUES
+(1, 'Prateek', 'Prakash', 'Manta', 9923433423, '2020-05-31 10:57:25', '2020-05-31 10:57:25', 0),
+(2, 'Pooja', 'Jitendra', 'Tripathi', 9829342232, '2020-05-31 10:58:43', '2020-05-31 10:58:43', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -168,16 +245,40 @@ CREATE TABLE `user_password` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `user_password`
+--
+
+INSERT INTO `user_password` (`Password_id`, `password`, `re_enter_password`, `created_on`, `updated_on`, `DELETED`) VALUES
+(1, '1234567', '1234567', '2020-05-31 11:01:18', '2020-05-31 11:02:21', 0);
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admin`
+-- Indexes for table `access`
 --
-ALTER TABLE `admin`
+ALTER TABLE `access`
+  ADD PRIMARY KEY (`Access_id`),
+  ADD KEY `F_id` (`F_id`),
+  ADD KEY `Admin_id` (`Admin_id`);
+
+--
+-- Indexes for table `admin_credentials`
+--
+ALTER TABLE `admin_credentials`
   ADD PRIMARY KEY (`Admin_id`),
   ADD KEY `a_id` (`A_id`),
   ADD KEY `u_id` (`U_id`);
+
+--
+-- Indexes for table `admin_form_access`
+--
+ALTER TABLE `admin_form_access`
+  ADD PRIMARY KEY (`Admin_form_id`),
+  ADD KEY `Access_id` (`Access_id`),
+  ADD KEY `Admin_id` (`Admin_id`),
+  ADD KEY `Form_id` (`F_id`);
 
 --
 -- Indexes for table `answers`
@@ -237,10 +338,22 @@ ALTER TABLE `user_password`
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT for table `access`
 --
-ALTER TABLE `admin`
-  MODIFY `A_id` int(16) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `access`
+  MODIFY `Access_id` int(16) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `admin_credentials`
+--
+ALTER TABLE `admin_credentials`
+  MODIFY `A_id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `admin_form_access`
+--
+ALTER TABLE `admin_form_access`
+  MODIFY `Admin_form_id` int(16) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `answers`
@@ -252,13 +365,13 @@ ALTER TABLE `answers`
 -- AUTO_INCREMENT for table `form`
 --
 ALTER TABLE `form`
-  MODIFY `F_id` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `F_id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `form_details`
 --
 ALTER TABLE `form_details`
-  MODIFY `Form_details` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `Form_details` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `questions`
@@ -270,29 +383,44 @@ ALTER TABLE `questions`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `U_id` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `U_id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user_details`
 --
 ALTER TABLE `user_details`
-  MODIFY `Details_id` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `Details_id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user_password`
 --
 ALTER TABLE `user_password`
-  MODIFY `Password_id` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `Password_id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1262236;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `admin`
+-- Constraints for table `access`
 --
-ALTER TABLE `admin`
-  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`);
+ALTER TABLE `access`
+  ADD CONSTRAINT `access_ibfk_1` FOREIGN KEY (`F_id`) REFERENCES `form` (`F_id`),
+  ADD CONSTRAINT `access_ibfk_2` FOREIGN KEY (`Admin_id`) REFERENCES `form` (`Admin_id`);
+
+--
+-- Constraints for table `admin_credentials`
+--
+ALTER TABLE `admin_credentials`
+  ADD CONSTRAINT `admin_credentials_ibfk_1` FOREIGN KEY (`U_id`) REFERENCES `user` (`U_id`);
+
+--
+-- Constraints for table `admin_form_access`
+--
+ALTER TABLE `admin_form_access`
+  ADD CONSTRAINT `admin_form_access_ibfk_1` FOREIGN KEY (`Access_id`) REFERENCES `access` (`Access_id`),
+  ADD CONSTRAINT `admin_form_access_ibfk_2` FOREIGN KEY (`F_id`) REFERENCES `access` (`F_id`),
+  ADD CONSTRAINT `admin_form_access_ibfk_3` FOREIGN KEY (`Admin_id`) REFERENCES `access` (`Admin_id`);
 
 --
 -- Constraints for table `answers`
@@ -306,7 +434,7 @@ ALTER TABLE `answers`
 -- Constraints for table `form`
 --
 ALTER TABLE `form`
-  ADD CONSTRAINT `form_ibfk_1` FOREIGN KEY (`Admin_id`) REFERENCES `admin` (`Admin_id`),
+  ADD CONSTRAINT `form_ibfk_1` FOREIGN KEY (`Admin_id`) REFERENCES `admin_credentials` (`Admin_id`),
   ADD CONSTRAINT `form_ibfk_2` FOREIGN KEY (`Form_details`) REFERENCES `form_details` (`Form_details`);
 
 --
@@ -319,8 +447,8 @@ ALTER TABLE `questions`
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`Details_id`) REFERENCES `user_details` (`details_id`),
-  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`Password_id`) REFERENCES `user_password` (`password_id`);
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`Details_id`) REFERENCES `user_details` (`Details_id`),
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`Password_id`) REFERENCES `user_password` (`Password_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

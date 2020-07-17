908eda33365729888d6da20e7481ba977a95618f
aniket0408 <singh.aniket0408@gmail.com>
2020-07-17 16:23:10 +0530

    copy working

--- pages/form_creation/send_data.php
+++ pages/form_creation/send_data.php
@@ -23,11 +23,12 @@ if(isset($_POST['question']) && isset($_POST['type']) && isset($_POST['rating'])
 	$control->deleteQuestion($question_id);
 }
 
-if(isset($_POST['copyquestion']) && isset($_POST['copytype']) && isset($_POST['copyoption1']) && isset($_POST['copyoption2']
+if(isset($_POST['copyquestion']) && isset($_POST['copytype']) && isset($_POST['copyscale']) && isset($_POST['copyoption1']) && isset($_POST['copyoption2']
 	) && isset($_POST['copyoption3']) && isset($_POST['copyoption4']) && isset($_POST['copyoption5'])) {
 
 	$copyquestion = $_POST['copyquestion'];
 	$copytype = $_POST['copytype'];
+	$copyscale = $_POST['copyscale'];
 	$copyoption1 = $_POST['copyoption1'];
 	$copyoption2 = $_POST['copyoption2'];
 	$copyoption3 = $_POST['copyoption3'];
@@ -35,7 +36,7 @@ if(isset($_POST['copyquestion']) && isset($_POST['copytype']) && isset($_POST['c
 	$copyoption5 = $_POST['copyoption5'];
 	include 'database-connection/model.class.php';
 	$model1 = new Model();
-	$model1->insertRecord($copyquestion, $copytype, $copyoption1, $copyoption2, $copyoption3, $copyoption4, 
+	$model1->insertRecord($copyquestion, $copytype, $copyscale, $copyoption1, $copyoption2, $copyoption3, $copyoption4, 
 		$copyoption5);
 	}
 

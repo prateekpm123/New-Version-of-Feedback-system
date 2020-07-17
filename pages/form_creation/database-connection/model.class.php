8d37ab14a53a501f447ed92a98bdac8e7f79ab15
aniket0408 <singh.aniket0408@gmail.com>
2020-07-17 14:54:25 +0530

    insert running

--- pages/form_creation/database-connection/model.class.php
+++ pages/form_creation/database-connection/model.class.php
@@ -3,15 +3,15 @@
 include 'dbh.class.php';
 
 	Class Model extends Dbh {
-		public function insertRecord($question, $type, $rating, $option1, $option2, $option3, $option4, $option5){
+		public function insertRecord($question, $type, $option1, $option2, $option3, $option4, $option5){
 			
 				session_start();
 				$F_id = $_SESSION['F_id'];
-					$query = " INSERT INTO `questions` (`F_id`,`Question_desc`, `type` , `rating_scale` , `Option1`, `Option2`,
+					$query = " INSERT INTO `questions` (`F_id`,`Question_desc`, `type`, `Option1`, `Option2`,
 															`Option3`, `Option4`, `Option5`) 
-								VALUES ('$F_id', '$question', '$type', '$rating' , '$option1', '$option2', '$option3', '$option4', '$option5' ) ";
+								VALUES ('$F_id', '$question', '$type' , '$option1', '$option2', '$option3', '$option4', '$option5' ) ";
 					$result = $this->connect()->prepare($query);
-					$result->execute([$question, $type, $rating, $option1, $option2, $option3, $option4, $option5]);
+					$result->execute([$question, $type, $option1, $option2, $option3, $option4, $option5]);
 				
 		}
 

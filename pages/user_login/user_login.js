f42fc25d0e3ed3710e6cc5fa7842f29f1b4fc1e3
prateekpm123 <prateekpm123@gmail.com>
2020-08-19 21:22:03 +0530

    added color to warning

--- pages/user_login/user_login.js
+++ pages/user_login/user_login.js
@@ -1,32 +1,32 @@
 function validate() {
 
     let email = $('#email').val();
-            let password = $('#pwd').val();
-            // console.log(email, password)
-            
-            // if(password.length >= 8)
-            // {
-                $('#password-warning').html(''); 
-                $.ajax({
-                    url: "backend/validate_user.php",
-                    type: "post", 
-                    data : {
-                        email : email,
-                        password : password,
-                    },
-                    success: function(data, status) {
-                        console.log(data);
-                        if(data == 1) {
-                            window.location.href = "../user_dashboard/user_dashboard.php";
-                        } else  {
-                            $('#validation-warning').html('<p class="warning">Wrong email or password</p>'); 
-                        }
-                        
-                    }    
-                });
-            // } else {
-            //     // $('#password-warning').html('<p class="warning">Enter password length more than 8 letters</p>'); 
-            // }
-            
+    let password = $('#pwd').val();
+    // console.log(email, password)
+    
+    // if(password.length >= 8)
+    // {
+        $('#password-warning').html(''); 
+        $.ajax({
+            url: "backend/validate_user.php",
+            type: "post", 
+            data : {
+                email : email,
+                password : password,
+            },
+            success: function(data, status) {
+                console.log(data);
+                if(data == 1) {
+                    window.location.href = "../user_dashboard/user_dashboard.php";
+                } else  {
+                    $('#validation-warning').html('<p class="warning" style="color: red;">Wrong email or password</p>'); 
+                }
+                
+            }    
+        });
+    // } else {
+    //     // $('#password-warning').html('<p class="warning">Enter password length more than 8 letters</p>'); 
+    // }
+    
 
 }

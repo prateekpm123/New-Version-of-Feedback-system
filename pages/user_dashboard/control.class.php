02bf322f6f9fdbedb5c3b94d334e3b82ceb5256c
aniket0408 <singh.aniket0408@gmail.com>
2020-08-22 19:37:00 +0530

    Minor Change in control class of user_feedback

--- pages/user_dashboard/control.class.php
+++ pages/user_dashboard/control.class.php
@@ -6,13 +6,29 @@ Class Control extends Dbh {
   public function insertAnswers($id, $value) {
     session_start();
     $user_email = $_SESSION['user_email'];
-    $query = "DELETE FROM answers WHERE User_email = ? AND Q_id = ? ";
-    $result = $this->connect()->prepare($query);
-    $result->execute([$user_email,$id]);
+    // $query = "DELETE FROM answers WHERE User_email = ? AND Q_id = ? ";
+    // $result = $this->connect()->prepare($query);
+    // $result->execute([$user_email,$id]);
 
+    $query = "SELECT * FROM answers WHERE User_email = '$user_email' AND Q_id = $id ";
+    $result = $this->connect()->prepare($query);
+    if ($result->execute([$user_email,$id])){
+      while ($rows = $result->fetch()){
+          $data = $rows;
+      }
+  }
+
+    if(!empty($data)){
+      $query1 = "UPDATE answers SET Answer_desc = '$value' WHERE User_email = '$user_email' AND Q_id = $id  ";
+      $result1 = $this->connect()->prepare($query1);
+      $result1->execute([$value,$user_email,$id]);
+    }
+
+    else {
     $query1 = "INSERT INTO answers (`User_email`,`Q_id`,`Answer_desc`) VALUES (?,?,?) ";
     $result1 = $this->connect()->prepare($query1);
     $result1->execute([$user_email,$id,$value]);
+    }
   }
 }
 
